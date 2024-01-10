ff50960900475ddb6336a5ce29ac1345e3d7c95a
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-11 00:31:46 +0100

    Some CSS Adjustments :)

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
-
 const videos = document.querySelector(".videos");
 
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			videos.classList.add("show");
+		} else if (!entry.isIntersecting) {
+			videos.classList.remove("show");
+		}
+	});
 });
 
 observer.observe(videos);
@@ -17,18 +15,16 @@ observer.observe(videos);
 const paragraphs = document.querySelector(".intro").querySelectorAll("p");
 
 paragraphs.forEach((paragraph, i) => {
-  if (i % 2 === 0) paragraph.style.textAlign = "left";
+	if (i % 2 === 0) paragraph.style.textAlign = "left";
 });
 
 //fixes the situation where the progress bar is not at 0% when the page is loaded
 window.addEventListener("DOMContentLoaded", () => {
-  progress.style.width = "0%";
+	progress.style.width = "0%";
 });
-
-
 
 const progress = document.querySelector(".progress");
 window.addEventListener("scroll", () => {
-  const max = document.body.scrollHeight - innerHeight;
-  progress.style.width = `${(window.scrollY / max) * 100}%`;
+	const max = document.body.scrollHeight - innerHeight;
+	progress.style.width = `${(window.scrollY / max) * 100}%`;
 });
